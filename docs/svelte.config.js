bc85cd7264d5068733e445c7ab032e150f3932c3
lolcabanon <julesses@gmail.com>
2024-02-25 23:04:54 +0100

    config docs for GitHub Pages

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,11 @@ const config = {
   kit: {
     adapter: adapter(),
     prerender: {
-      handleMissingId: 'warn',
-    },
+      handleMissingId: 'warn'
+    }
   },
 
-  preprocess: [vitePreprocess({})],
+  preprocess: [vitePreprocess({})]
 };
 
 export default config;
